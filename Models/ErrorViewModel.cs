aa7a8c6fe3c46e25f661418d135ae9d4fb8d5d0c
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-26 10:13:12 +0545

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FeedbackWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ feedback.cs
@@ -0,0 +1,11 @@
+﻿namespace FeedbackWebApplication.Models
+{
+    public class feedback
+    {
+        public int Id { get; set; }
+        public string Answer { get; set; }
+        public string Rating { get; set; }
+        public string Sentiment { get; set; }
+        public DateTime SubmittedAt {  get; set; }
+    }
+}
